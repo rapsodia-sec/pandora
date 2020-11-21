5536abc76c475401cf150bc421b8fc0ee71e0368
rapsodia <mattiadallantonia@gmail.com>
2020-11-21 17:48:05 +0100

    Initial commit

---
+++ model/user.py
@@ -0,0 +1,14 @@
+class User():
+    def __init__(self,
+                 id=None,
+                 username=None,
+                 email=None):
+        self.id = id
+        self.username = username
+        self.email = email
+
+    #@classmethod
+    #def create(cls,
+    #          username,
+     #          email,
+      #         password):
